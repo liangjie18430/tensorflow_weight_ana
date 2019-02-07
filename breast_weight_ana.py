c7a67a9d24ad0933461d12bad5b0d01d40f60058
liangjie <liangjie001@ke.com>
2019-02-07 17:06:13 +0800

    add some metric

--- breast_weight_ana.py
+++ breast_weight_ana.py
@@ -13,6 +13,7 @@ from tensorflow.python import pywrap_tensorflow
 
 def two_example():
     count = 0
+    result_dict = dict()
     with tf.Graph().as_default():
         with tf.Session() as sess:
             # br_logs是用breast的dataframe训练wide and deep所产生的文件
@@ -26,16 +27,44 @@ def two_example():
                     weight_value = reader.get_tensor(key)
 
                     if (key.endswith("weights")):
-                        print("variable name: ", key)
-                        print("weight_shape: ", np.shape(weight_value))
+                        # print("variable name: ", key)
+                        # print("weight_shape: ", np.shape(weight_value))
                         print(weight_value)
                         count = count + 1
+                        result_dict[key] = weight_value
 
 
             else:
                 print('No checkpoint file found')
     # count和breast的shape中的column 列数一致，breast中包含label
     # count中有针对bias_weight的统计，都等于len(feature_columns)+1
-    print("count: ",count)
+    print("count: ", count)
+
+    return result_dict
+
+
+def is_has_big_weight(result):
+    """
+    判断是否有大于1的权重
+    :return:
+    """
+    bigger_than_one = dict()
+    for k in result.keys():
+        weights = result[k]
+        for weight in weights:
+            if isinstance(weight, list) or isinstance(weight, np.ndarray):
+                for wi in weight:
+                    if wi > 1:
+                        bigger_than_one[k] = weights
+            elif weight > 1:
+                bigger_than_one[k] = weights
+
+    # 输出大于1的权重
+    for k in bigger_than_one.keys():
+        v = bigger_than_one[k]
+        print("k: ", k, ", v: ", v)
+
+
 if __name__ == '__main__':
-    two_example()
+    result = two_example()
+    is_has_big_weight(result)
